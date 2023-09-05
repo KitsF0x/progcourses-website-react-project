ef06b096c224e72e4e22f593abf58831afd75e53
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-09-05 20:48:58 +0200

    Create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import Navigation from "./components/Navigation";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="hero-img">
+        <div className="hero-text">
+          <h1>
+            Witamy w Prog<span className="title-blue">Courses</span>
+          </h1>
+          <p>Najlepszych kursach programowania w Internecie</p>
+        </div>
+        <div className="hero-shadow"></div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
